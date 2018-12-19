f2ecf8e76ae75858d71b411113df945c1533649e
Jose Aldeguer Sastre <jas53@alu.ua.es>
2018-12-19 16:28:55 +0100

    Añadir area rectangulo

---
+++ code/figures.c
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+void rectangle();
+
+
+main(){
+  int option=1;
+
+  do{
+    cout<<"Select an option: "<<endl;
+    cout<<"1. Area of a rectangle"<<endl;
+    cin>>option;
+  }while(option!=1);
+  cout<<"Option: "<<option<<endl;
+  cout<<endl;
+  switch(option){
+      case 1: rectangle();
+      break;
+  }
+}
+
+void rectangle(){
+  float base=0, height=0, area=0;
+  
+  cout<<"Base: ";
+  cin>>base;
+  cout<<"Height: ";
+  cin>>height;
+  area=base*height;
+  cout<<area<<endl;
+}
