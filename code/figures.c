8b8491f8434ecdb7b189f46978acdd1732cca694
Jose Aldeguer Sastre <jas53@alu.ua.es>
2018-12-19 16:33:11 +0100

    Añadir area triangulo"

--- code/figures.c
+++ code/figures.c
@@ -2,7 +2,7 @@
 using namespace std;
 
 void rectangle();
-
+void triangle();
 
 main(){
   int option=1;
@@ -10,13 +10,16 @@ main(){
   do{
     cout<<"Select an option: "<<endl;
     cout<<"1. Area of a rectangle"<<endl;
+    cout<<"2. Area of a triangle"<<endl;
     cin>>option;
-  }while(option!=1);
+  }while(option<1 || option>2);
   cout<<"Option: "<<option<<endl;
   cout<<endl;
   switch(option){
       case 1: rectangle();
-      break;
+    break;
+      case 2: triangle();
+    break;
   }
 }
 
@@ -29,4 +32,15 @@ void rectangle(){
   cin>>height;
   area=base*height;
   cout<<"The area of the rectangle is: "<<area<<endl;
+}
+
+void triangle(){
+  float base=0, height=0, area=0;
+  
+  cout<<"Base: ";
+  cin>>base;
+  cout<<"Height: ";
+  cin>>height;
+  area=(base*height)/2.0;
+  cout<<area<<endl;
 }
